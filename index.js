8a7713608e9be5216e0f4b8caf14381b22c47892
Yan <y.aliasov@ips.su>
2022-04-29 18:29:41 +0300

    widget

--- index.js
+++ index.js
@@ -16,51 +16,22 @@ async function widgetScript() {
     for(const element in alfaPaymentData) {
         if(alfaPaymentData[element].indexOf('.') === 0) {
             const foundSelector = document.querySelector(alfaPaymentData[element]);
-            if(foundSelector === null) {
-                console.error(`Element with ${foundSelector} class not found`);
-                throw new Error(`Element with ${foundSelector} class not found`);
-            } else {
-                alfaPaymentData[element] = foundSelector.value
-            }
+            alfaPaymentData[element] = (foundSelector.value || foundSelector.innerText).replace(/ /g, '-');
         }
     }
 
-    const requestData = {
-        order: {
-            // STRING && should not be empty
-            token: alfaPaymentData?.token,
-            // must be shorter than or equal to 128 characters && STRING && should not be empty
-            number: alfaPaymentData?.orderNumberSelector,
-        },
-        cost: {
-            // must be a number conforming to the specified constraints && should not be empty
-            amount: Number(alfaPaymentData?.amountSelector),
-        },
-        customer: {
-            name: alfaPaymentData?.clientInfoSelector,
-            email: alfaPaymentData?.emailSelector,
-        },
-        description: {},
-        postdata: {},
+    // #transform amount
+    // TODO
+
+    // #covert amount by amount format
+    if(alfaPaymentData.amountFormat === 'kopeyki') {
+        alfaPaymentData.amountSelector *= 100;
     }
 
-    // testing request to register (https://test.egopay.ru/send_link/api/register)
-    if(!requestData.order.token && requestData.order.token !== '') {
-        try {
-            const request = await fetch('https://test.egopay.ru/send_link/api/register', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                mode: 'cors',
-                body: JSON.stringify(requestData)
-            })
-            const response = await request.json();
-        } catch(error) {
-            console.error(error);
-            throw new Error(error);
-        }
-    } else {
-        throw new Error('Token is not defined');
-    }
+    const params = new URLSearchParams(alfaPaymentData).toString();
+
+    const request = await fetch(`https://test.egopay.ru/api/ab/rest/?${params}`, {
+        method: 'POST',
+    });
+    const response = await request;
 }
