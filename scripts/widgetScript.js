ab6ebe41a1b21cfcee67a1084c6cca53e2db609d
Yan <y.aliasov@ips.su>
2022-05-05 16:02:12 +0300

    fix regex

--- scripts/widgetScript.js
+++ scripts/widgetScript.js
@@ -17,6 +17,16 @@ export default async function widgetScript() {
     // # transform amount
     transformAmount(alfaPaymentData);
 
+
+    const { valid, errorMessages } = validation(alfaPaymentData);
+
+    if(!valid) {
+        console.log('>>errorMessages', errorMessages);
+        return errorMessages;
+    }
+
+    console.log('valid');
+
     // try {
     //     const request = await fetch(`https://test.egopay.ru/api/ab/rest/`, {
     //         method: 'POST',
@@ -26,6 +36,23 @@ export default async function widgetScript() {
     // } catch(error) {
     //     console.error(error);
     // }
+}
+
+function validation(data) {
+    let valid = true;
+    const errorMessages = {};
+
+    // # сумма введена, 
+    // # сумма больше и не равна нулю
+    if(!(Number(data.amount) && Number(data.amount) > 0 && Number(data.amount) !== 0)) {
+        valid = false;
+        errorMessages.amountError = 'Поле сумма не заполнено';
+    }
+
+    return {
+        valid,
+        errorMessages
+    };
 }
 
 function findDataFromElements(data) {
@@ -54,8 +81,9 @@ function transformAmount(data) {
     data.amount = data.amount.replace(/,/g, '.');
 
     if (data.amount.includes('руб')) {
-        const regExp = new RegExp(/(\d+)+руб(лей|ля|ль|\.|\d)(\d+)+коп(еек|ейки|йка|\.|\s)?/g);
+        const regExp = new RegExp(/(\d+)+руб(лей|ля|ль|\.|\d|)((\d+)+коп(еек|ейки|йка|\.|\s)|)?/g);
         data.amount = data.amount.replace(regExp, (match, rubles, rub_ending, copecks) => {
+            console.log(match, rubles, rub_ending, copecks);
             return rubles + rub_ending + copecks;
         })
     }
